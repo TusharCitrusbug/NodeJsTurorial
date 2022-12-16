0d451a635bb0e702b98031740fc75910e2337c21
TusahrCitrusbug <tushar.citrusbug@gmail.com>
2022-12-16 13:26:24 +0530

    added a websocket for real time message

--- index.js
+++ index.js
@@ -1,92 +1,19 @@
-// const data = require('./data')
-// const http = require('node:http');
-// const port = 8000;
-// const hostname = 'localhost';
-
-// const server = http.createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'application/json');
-//     res.write(JSON.stringify(data.data));
-//     res.end()
-// });
-
-// server.listen(port, hostname, () => {
-//     console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
-
 const express = require('express')
+const http = require('http')
+const port = 6969;
+const server = http.createServer(express)
+const WebSocket = require('ws')
+const web_socket_server = new WebSocket.Server({ server });
 
-const environ = require('./environ')
-
-const app = express();
-
-const cookie_parser = require('cookie-parser');
-const statis_path = environ.path.static_path
-// app.use(express.static(environ.path.static_path))
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: false }))
-
-// custom middle ware in express
-const customMiddleWear = (req, resp, next) => {
-    next();
-    // if (req.body.fname && req.body.fname) {
-    //     next();  
-    // }else{
-    //     resp.render('404')
-    // }
-}
-app.use(customMiddleWear)
-app.use(cookie_parser())
-
-// app.get('', (req, resp) => {
-//     resp.sendFile(`${statis_path}/index.html`)
-// })
-
-
-// render direct with ejs
-
-app.get('', (req, resp) => {
-    resp.render('index')
-})
-
-app.post('/home', (req, resp) => {
-    console.log(req.body);
-    resp.redirect('/')
-})
-app.get('/about', (req, resp) => {
-    let data = {
-        tushar: "lsjkdlskd",
-        rahul: "kdfjkdfjdkf",
-        ratan: "xmcnxmcnxmc",
-        manan: "smdskjdksjdskdjsd",
-        subjects: ["skdd", 's', 12, 5, 9, "kjdskjsdk"]
-    }
-    resp.render('users/about', { data })
-})
-
-// render direct html file
-// app.get('*', (req, resp) => {
-//     resp.sendFile(`${statis_path}/404.html`)
-// })
-
-// Cookies in node/express js
-app.get('/set-cookies', (req, resp) => {
-    resp.cookie('foo','bar')
-    resp.send("cookie is set");
-
-})
-app.get('/get-cookies', (req, resp) => {
-    console.log(req.cookies);
-    resp.send(req.cookies);
-})
-app.get('/remove-cookies', (req, resp) => {
-    resp.clearCookie("foo")
-    resp.send(req.cookies);
+web_socket_server.on('connection', (ws) => {
+    ws.on("message", (data) => {
+        web_socket_server.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(data)
+            }
+        })
+    })
 })
 
 
-app.get('*', (req, resp) => {
-    resp.render('404')
-})
-app.listen(8000);
+server.listen(port)
